a085496c5a6c791f0f6be30c30ec2426bab648d6
harshilv17 <harshilvalecha@gmail.com>
2026-06-07 18:07:52 +0530

    fresh start

--- assets/password-modal.js
+++
@@ -1,8 +0,0 @@
-class PasswordModal extends DetailsModal {
-  constructor() {
-    super();
-
-    if (this.querySelector('input[aria-invalid="true"]')) this.open({ target: this.querySelector('details') });
-  }
-}
-if (!customElements.get('password-modal')) customElements.define('password-modal', PasswordModal);
